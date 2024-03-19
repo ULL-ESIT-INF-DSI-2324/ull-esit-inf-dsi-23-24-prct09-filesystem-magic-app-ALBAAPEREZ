417a8fda190eeaf9f918005c1157b8c96fb897c2
Alba Perez <alu0101513768@ull.edu.es>
2024-03-19 23:12:16 +0000

    comienzo de la practica

--- src/EJERCICIO.ts
+++ src/EJERCICIO.ts
@@ -1,39 +1,12 @@
-// Ejercicio de ejemplo
-// src/main.ts
-
-export abstract class Animal {
-  constructor(public name: string) {}
-
-  abstract makeSound(): string;
+/**
+ * Adds two __numbers__
+ * @param firstNumber Consists of the first operand of the addition
+ * @param secondNumber Consists of the second operand of the addition
+ * @returns The addition of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * add(1, 7) = 8
+ * ```
+ */
+export function add(firstNumber: number, secondNumber: number) {
+  return firstNumber + secondNumber;
 }
-
-export class Dog extends Animal {
-  makeSound() {
-      return "Woof!";
-  }
-}
-
-export class Cat extends Animal {
-  makeSound() {
-      return "Meow!";
-  }
-}
-
-export class AnimalSound {
-  constructor(public animal: Animal) {}
-
-  play() {
-      return this.animal.makeSound();
-  }
-}
-
-function main() {
-  const dog = new Dog("Rex");
-  const cat = new Cat("Whiskers");
-  const dogSound = new AnimalSound(dog);
-  const catSound = new AnimalSound(cat);
-  console.log(dogSound.play());
-  console.log(catSound.play());
-}
-
-main();
--- tests/Ejercicio.spec.ts
+++ tests/Ejercicio.spec.ts
@@ -1,19 +1,13 @@
-// PRUEBAS DE EJEMPLO
-
 import 'mocha';
-import { expect } from 'chai';
-import { Dog, Cat, AnimalSound } from '../src/EJERCICIO';
+import {expect} from 'chai';
+import {add} from "../src/EJERCICIO.js";
 
-describe('Ejercicio de ejemplo', () => {
-  it('Perro hace Woof!', () => {
-    const dog = new Dog("Rex");
-    const dogSound = new AnimalSound(dog);
-    expect(dogSound.play()).to.equal("Woof!");
+describe("add function tests", () => {
+  it("add(1, 8) returns value 9", () => {
+    expect(add(1, 8)).to.be.equal(9);
   });
 
-  it('Gato hace Meow!', () => {
-    const cat = new Cat("Whiskers");
-    const catSound = new AnimalSound(cat);
-    expect(catSound.play()).to.equal("Meow!");
+  it("add(-1, 8) returns value 7", () => {
+    expect(add(-1, 8)).to.be.equal(7);
   });
 });
